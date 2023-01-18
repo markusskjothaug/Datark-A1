5d8370322014e878a9ab40828d7bd3880dadc41e
markusskjothaug <60712562+markusskjothaug@users.noreply.github.com>
2023-01-18 13:13:56 +0100

    x

--- main.c
+++ main.c
@@ -13,8 +13,7 @@
 #define F_CPU 1000000
 #include <util/delay.h>
 
-const NUMBERTOWIN = 5;
-const input_pushed = 0b00100000;
+const int NUMBERTOWIN = 3;
 
 // Making functions that runs if a spesific user wins the game
 
@@ -33,9 +32,9 @@ void Victory_A(){
 
 void USR_B_Win(){
 	for (int i = 0; i < 10; i++){
-		PORTB = 0b00011111;
+		PORTC = 0b00011111;
 		_delay_ms(500);
-		PORTB = 0b00000000;
+		PORTC = 0b00000000;
 	}
 }
 void Victory_B(){
@@ -51,21 +50,21 @@ int main(void)
 	int USR_A_TOT_WINS = 0;
 	
 	//USR_B parametres/variables
-    DDRB = 0b00011111;
-	PORTB = 0b00000000;
+    DDRC = 0b00011111;
+	PORTC = 0b00000000;
 	int USR_B_Count = 0;
 	int USR_B_TOT_WINS = 0;
 
     while ((USR_A_TOT_WINS != 4) | (USR_B_TOT_WINS != 4)){
 		/*USR_A: if USR_A pushes the button, the lights will light up one at a time, and the lights will stay on.
 		* It also increments the value needed to win the game. */
-		if (PORTA &= input_pushed){
+		if (PORTA &= 0b00100000){
 			PORTA = (PORTA << 1) + 0b00000001;
 			USR_A_Count++;
 		}
 		//USR_B
-		if (PORTB &= input_pushed){
-			PORTB = (PORTB << 1) + 0b00000001;
+		if (PORTC &= 0b00100000){
+			PORTC = (PORTC << 1) + 0b00000001;
 			USR_B_Count++;
 		}
 		// @markusskjothaug
